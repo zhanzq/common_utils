df6013bf530fa1a4f24c6597a3453ea15d1f8f97
zhanzq <zhanzhiqiang09@126.com>
2022-12-08 15:06:05 +0800

    使用前缀和后缀分割，优化编辑距离计算

--- common_utils/algorithm/edit_distance.py
+++ common_utils/algorithm/edit_distance.py
@@ -6,7 +6,7 @@
 #
 
 
-def get_edit_path(dp):
+def get_edit_path(dp, pre_len=0, pst_len=0):
     """
     according to edit distance matrix, return the path,
     'i': insert a new element into second
@@ -14,6 +14,8 @@ def get_edit_path(dp):
     'r': replace the element in first with the one in second
     's': keep the same
     :param dp: the input edit distance matrix
+    :param pre_len: the prefix length, default 0
+    :param pst_len: the postfix length, default 0
     :return:
     """
     i = len(dp) - 1
@@ -42,7 +44,7 @@ def get_edit_path(dp):
                 i -= 1
                 j -= 1
 
-    edit_path = edit_path[::-1]
+    edit_path = "s"*pre_len + edit_path[::-1] + "s"*pst_len
 
     return edit_path
 
@@ -95,6 +97,7 @@ def get_edit_distance_matrix(source, target):
     :param target: with type **str** or **str list**
     :return:
     """
+
     sz1 = len(source)
     sz2 = len(target)
 
@@ -115,6 +118,53 @@ def get_edit_distance_matrix(source, target):
                 dp[i + 1][j + 1] = min(dp[i][j + 1] + 1, dp[i + 1][j] + 2, dp[i][j] + 4)
 
     return dp
+
+
+def get_edit_distance_matrix_with_pre_pst(source, target):
+    """
+    calculate edit distance matrix between source and target,
+    :param source: with type **str** or **str list**
+    :param target: with type **str** or **str list**
+    :return: edit distance matrix **dp**, prefix length **pre_len** and postfix length **pst_len**
+    """
+    pre_len, pst_len = 0, 0
+
+    sz1 = len(source)
+    sz2 = len(target)
+
+    i = 0
+    while i < sz1 and i < sz2:
+        if source[i] == target[i]:
+            pre_len += 1
+        else:
+            break
+    j = 1
+    while j <= sz1 and j <= sz2:
+        if source[-j] == target[-j]:
+            pst_len += 1
+        else:
+            break
+
+    source1, target1 = source[pre_len:-pst_len], target[pre_len:-pst_len]
+    sz1, sz2 = len(source1), len(target1)
+
+    dp = [[0 for _ in range(sz2 + 1)] for _ in range(sz1 + 1)]
+
+    # initialize
+    for i in range(sz1 + 1):
+        dp[i][0] = i
+
+    for j in range(sz2 + 1):
+        dp[0][j] = j
+
+    for i in range(sz1):
+        for j in range(sz2):
+            if source1[i] == target1[j]:
+                dp[i + 1][j + 1] = dp[i][j]
+            else:
+                dp[i + 1][j + 1] = min(dp[i][j + 1] + 1, dp[i + 1][j] + 2, dp[i][j] + 4)
+
+    return dp, pre_len, pst_len
 
 
 def _print_edit_distance_matrix(dp):
