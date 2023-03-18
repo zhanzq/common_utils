263c5d86a1b1a40b29c83525ea020d80881e423a
23001141 <zhanzhiqiang@haier.com>
2023-03-18 18:39:25 +0800

    增加__init__文件，使得对应的package在发布时可见

---
+++ common_utils/haier/auto_test/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# encoding=utf-8
+# author: zhanzq
+# email : zhanzhiqiang09@126.com 
+# date  : 2023/3/18
+#
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
