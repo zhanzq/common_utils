eb221f4029272014fbe1b34922bf9faa7e57f8ad
zhanzq <zhanzhiqiang09@126.com>
2024-03-20 14:42:16 +0800

    1. 支持向玄武系统中新增字典add_word_dict_in_xuanwu

--- common_utils/haier/xuanwu/dict_op.py
+++ common_utils/haier/xuanwu/dict_op.py
@@ -140,7 +140,60 @@ def edit_xuanwu_dict(dict_code, word, synonym):
     return obj_resp
 
 
+def add_word_dict_in_xuanwu(dict_code, dict_name, parent_code=0, dict_type="ENUM", dict_category="OTHER"):
+    """
+    向玄武系统中新增字典
+    :param dict_code: 玄武字典代码
+    :param dict_name: 字典名称
+    :param parent_code: 父节点代码，默认为0
+    :param dict_type: 字典类型，默认为ENUM，枚举类型
+    :param dict_category: 字典分类，默认为OTHER
+    :return:
+    """
+    url = "https://aidev.haiersmarthomes.com/xuanwu-admin/nlp/dictionary/add"
+
+    headers = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Cookie": "JSESSIONID=9a4d7b0c-b390-43e5-b6cc-e4e9740d7277; rememberMe=van4gr52Zk3ciihKgeKpbKPvQCZ5PcItlfr+uH4XBs1rJRY6KiCeaZ/qvhSg69IIMgkL9c/SGhQGEJw/gh/3mtA6HF9lRcDt/K3WcINZA2HHyg0jLi8+CNkAZ7e4GWcwQ0m9XrcsVtVuLtRYRGJOKFe9V4vbf+WWD3lINQ+EVRnj4jA/Udd3V3nVg3J8MnQ7Jgt9y4G23dEpHY/31bB4/PnSky64e9aunu1c/ZT359ORHpPIlbsltxZvyjYAXD/oXAa8zvVn7uUqscW3Z4GgINZgj++K5wuJSLKemGbQIEwS3y3TeCzs3FTAQ9Sh2PpPsaeqYeiduOVixfp/RhoEhrmwcTMgoD/Hlv+4p/mJIClmZrGiRAz/egcs+JkewKL+JRF02vw4VzPewl4O8Cg/0/kU71N2GZvSxbTLDZ5QoshlH5bJP+WY8sO4CH0qN9vysDgLkifH6DpWtUusB7Kp5RHRp4Yp6BxxyMUXPxnvacaca759LIgrlSwkRISooll+HlY1nM6IVld+ck6uoLSdCCV1Kbs+1tCeLGgFYenltX1yf3QHYSY7j57ez4M5TF8Z23KuBxXkdI+sic25nDiBZCHHDB2gAWKbEokfjiZI8JMt/mXXhKGzwSTU9JW8kN2Vm43aQK6OK/A6iOZnW+s1NQODwbfdAyA8qH9e2YhQzg25BSG6j8vXBVToVQ9RPilP/HDddFCeBxNa5mD99hrzdFcq47qfLP00miZBAKO6m4XfuDzgbmuK8UZK8y9PbL8PGeaSItmQ8Et4ZsYnlJf4sYDKon0Trv/UMsumvqZkkEdFWVFoGVqgeakb0JvgOzIu+xJIxdFE6q3xhN9mKTeErIJgVa4tgBA7g6YETPsPsVGqFSH9WeoqYmkH9CUSKWWGZOFkk1HngulO97RvVKD0vxnrKsPs9dXMiWXfcY1YthIFUD5A6hT/1cJm3LF0N1hN6JtEo2Wnxhl4Iom1djClZw68iwjcuD+s5ZkHblaYffvUBzDjGpqbdkNIT0XXSdh4uTtApOqjpCuUINdPqMQHghLsI6xf3YwZYcK0xHWcwfTLhmbvN1RXrCDxjhNgp4RYcBRc1MrCevJrymlDwbLNE4e7qqXBQ6Q5vY5Nppp/bK+QZFhLk93MoH4Pia4QQjLU6bIcATSipVCqh+Qw1BnOrJm4K5D5KbVg7vQHe26uNtPGJahcCvZXUtvklFUtAIHs9AUCfcGD9gh7pSh3aSeyGULpBK5Ev+xeADjoMFkORdnonhfhAPZBN/BxECjAkukwMWT+F2bIX4nFr62360BZpyFlk2dZmCBicHxFiNtscziocWq/ti/sJeZxEag1sK9S5hygWvxpM1oYTmEDMDCKPnbw6nbvqdCpaW+uWkU6PwWeaqflwLnWxSIWGbDhkRkzIFeAAU+yGQsptYrVCIMoL2iMqWy7ZfN077a1f3Kq127NoKsPIuz7rs+ABR2hhLzLPoi0gpLOED1MhEZLzb0GbZy+GSioMez/sbB63Jt9Ki+8nKQeWGQQkIp0mjBhQTxTvCVgkhvhE7/KBIDRcu6XGvqMrVhP+dmmwvRuwHWhYYuoTCvoSQrp+apduXJ5La4Hh1FjEpxC1NX2RuBWi743dSd2Y0n0Y55R+bcmmptFaSd4FGyjM+VvhDb8xXuDlTPkurZiYYkQ2ATjyHK6mqwuksz19BH4o5NjF4Fk03duR1sy8FmqUGLqcA3GNp3JNTd/Ujpwn3PvzMs9qik8xt43iBRBrs/Z/41VK9xeCEKMA39ORsSdn9yc5wGncU95iJhe2p0H2LGlwwuMmL/qci85WQStXHEvUfpquDW6cG849IOfUFri7XmpS+APaWGKZVoB/lEgvtT5TQ5pKMHKHHSSygXEXX2Cz2O9NkdvBem03CXiFMtq8bzc3X0RHIc2mApbbKOrYvf+pxgVZaBPQawEcw9waIyVUYCMaPpQeCVdNBEn8KdLtjz160pKtUaE4hN5yDuYStnymInpaZYHEyKhB0/6FOTnnKUtiOUq+kcorSetIT3nE2NUx0rTnLbx8Rl3rB2FjJUct/uhWPtiUPgD9gnWWp50m38solFzxVp95HHqcs+Ui7tm58QKAzaz50yiIi0Bs8znlT2IDkL6aKPoSkEyMWh6wGW4g2D5TERETHgGSis0hIKEg85oUx1VrvZkkgbKgXnPnBEJzjvm3wnGPpH4hfFbm6uKEs6LCdOxzJRNNBhnmGtR7ECP66n1oMl+y1IHCNNjSQTndCceE0RubU/pnXCkG5/buG0rSir6YgE/f8FZ/ehmTtNCeFiO2RBYCg6hbC4jjXtVtAOIq3mCOOlfT/8xnx+aGDw/QoRK54q0YtvTBqZ2dThlnFbe+6pU95ATBrxszXIcRYXDMRiCAckQspRxd1veb5qbeqyGI3bcOqBChBqdS5bjZqyeHg8iZP3w3nC/aRZkq8yA3WmKbl/cNVZHHj9jS5nOSVxfBahcYTx3K8nDA0GLuqXfGzqag/odSr46x1XyZeRUiPa/KOfX5yuH+pRZhTq0t/NI76OdIxOb6tMCdoWgYNeXprmQz412K2TltKtT68iU4qS0QY3R14BnyaN6+/gqgMpvlu3oainQnsGCJwIyQVDEJVpp6/opflST6hxM7K66lHpTxYSzJ9KhdJNKPdoCta0=",
+    "Origin": "https://aidev.haiersmarthomes.com",
+    "Pragma": "no-cache",
+    "Referer": "https://aidev.haiersmarthomes.com/xuanwu-admin/nlp/dictionary/add",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+    "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\""
+}
+    method = "POST"
+    # payload = (f"dictCode={dict_code}&dictName={quote(dict_name)}&parentCode={parent_code}&dictType={dict_type}"
+    #            f"&dictCategory={dict_category}")
+
+    data = {
+        "dictCode": dict_code,
+        "dictName": dict_name,
+        "parentCode": parent_code,
+        "dictType": dict_type,
+        "dictCategory": dict_category
+    }
+    payload = json.dumps(data)
+    response = requests.request(method, url, headers=headers, data=payload)
+    obj_resp = json.loads(response.text)
+
+    return obj_resp
+
+
 def main():
+    # add word dict
+    res_info = add_word_dict_in_xuanwu(dict_code="english_song", dict_name="英文歌名称")
+    print(res_info)
+
     # show word dict
     dict_code = "set_to"
     word_dct, _ = get_word_dict_in_xuanwu(dict_code=dict_code)
