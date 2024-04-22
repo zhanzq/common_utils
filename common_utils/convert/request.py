a2b0c1247062cc9aebcea18381bea0d71fb7aea7
zhanzq <zhanzhiqiang09@126.com>
2024-04-22 15:19:24 +0800

    1. 修复curl请求中的数据抽取功能的bug

--- common_utils/convert/request.py
+++ common_utils/convert/request.py
@@ -12,7 +12,7 @@ import json
 def __parse_curl_code(curl_code):
     # replace format char
     curl_code = re.sub(string=curl_code, pattern="\s", repl=" ")
-    ptn = "(curl |-H |--header |--location |--data-raw |--data |--compressed)"
+    ptn = "(curl |-H |--header |--location |--data-raw |--data |--compressed|--insecure)"
     items = re.split(string=curl_code, pattern=ptn)
 
     return items
@@ -37,12 +37,18 @@ def __extract_data(items):
             data = items[idx + 1]
             try:
                 data = data.strip()
+                if data.startswith("'") or data.startswith("\""):
+                    data = data[1:-1]
+                    data = data.replace("false", "False").replace("true", "True").replace("null", "None")
                 data = eval(data)  # 去除两端可能的'或者"
+                if type(data) is dict:
+                    return data
             except Exception as e:
                 print(e)
                 idx += 1
                 continue
-            if data and "=" in data and "&" in data:
+            # if data and "=" in data and "&" in data:
+            if data and "=" in data:
                 return data
             else:
                 out.update(json.loads(data))
@@ -99,6 +105,7 @@ def __is_url(s):
 def __preprocess_url_code(url_code):
     url_code = url_code.replace("--location", "")
     url_code = url_code.replace("--compressed", "")
+    url_code = url_code.replace("--insecure", "")
     return url_code
 
 
@@ -129,9 +136,16 @@ def convert_curl_to_python_request(curl_code):
     items = __parse_curl_code(curl_code)
     url = __extract_url(items)
     headers = __extract_headers(items)
+    headers_str = json.dumps(headers, ensure_ascii=False, indent=4)
+    headers_lst = headers_str.split("\n")
+    headers_lst = ["    " + it for it in headers_lst]
+    headers_str = "\n".join(headers_lst).strip()
     data = __extract_data(items)
     if type(data) is dict:
-        data = "\"\"\"{json.dumps(data, indent=4, ensure_ascii=False)}\"\"\""
+        data = json.dumps(data, indent=4, ensure_ascii=False)
+        data_lst = data.split("\n")
+        data_lst = ["    " + it for it in data_lst]
+        data = "\n".join(data_lst).strip()
 
     codes = f"""
 import requests
@@ -140,19 +154,16 @@ import json
 
 def do_request():
     url = "{url}"
-
-    headers = {json.dumps(headers, ensure_ascii=False, indent=4)}
+    headers = {headers_str}
     method = "GET"
     payload = ""
-    data = "{data}"
+    data = {data}
     if data:
         payload = data
         method = "POST"
 
     response = requests.request(method, url, headers=headers, data=payload)
-
     obj_resp = json.loads(response.text)
-    print(obj_resp)
 
     return obj_resp
 
@@ -165,26 +176,24 @@ do_request()
 
 
 def main():
-    curl_code = """curl 'https://aidev.haiersmarthomes.com/xuanwu-admin/ver/deployTask/add' \
-  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+    curl_code = """curl 'https://aidev.haiersmarthomes.com/xuanwu-admin/jwt/domainIntent/add' \
+  -H 'Accept: application/json, text/plain, */*' \
   -H 'Accept-Language: zh-CN,zh;q=0.9' \
   -H 'Cache-Control: no-cache' \
   -H 'Connection: keep-alive' \
-  -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
-  -H 'Cookie: visible=false; username=zhanzhiqiang; JSESSIONID=29d20a79-0348-4a78-8a72-daa6216d1a54; rememberMe=NtUu4YdhkyhZXtLIY7Eo9YRuoRZ6vh7Zkbf3N6j4kvQkL70aaV8iheYtE94Luy93cjgumTUJi9ReTgBWVrHY3jZHnEDrvgxJphyWg6uMBIpZb24FvgldKioqhQ9Ay4+sPbFqvX/U80h4z2WH5pJ2NFt3ReIKjtX6WlzIHNETW7EtlmXq7GsHfRTl8nKKewQOuBiXuRSnpgfIo1KtEEvh4aMxHLp4pA95zzP+ehpaRfD2u1xv/kWmqmW2Q88z5Mv1SP1IkJaTGn+SICIRAfAX1IA5jkhC3sX5AavxA+PUjo4RG2LI1RkdLEC0OT4HACKPAQ32Blv5kP/wcByFIiAqXyUrccBYEJS+Iq3HHlyc+XsVuKrqPvuoYarvqHbssdrei1UTQovg+c7nLuRLJ60BB+KZjL04C8Zg9bgi39iiMC7ZWPskpPpsGdyZYoRZ03sja40phMQSSU0x/laAf/B5w7mUhb0+0j95VpFIrQopGtOfmlnR2+CCxWuX8Bd1pyIAMRAhLybCe7CIwN2F1910OA4ylzPAsgYqeLMtBLUR4iTF2abt6faOuvx5S00rRrBDOIO7TOpWVUwCtu+tXRCtEWxXFQtYS888BrWHEQ6bLPtwAXIKkIuRd/oFwEqTuPmINYO2sdO8zLhg1qIF2EwUqlebZvD7djbVvLeuzWOP5B/D1ON1f5HrI+VRdKexl/moCyjEiBfca3g3v5aIOM2GTGodvF1Gt2JtOLUq261XdX4KK/vZ1pJnxPEH3m8+yBkvmeZPB5a+3P2Ovp2NWP9r6pz2ottcUYB0FMZjK43YXU0TlZsl2vGGLSyCt0mkn3WvTXYSt7/TgeZPKzYSKOmQNCulGY2rh7I7LwQv3krGmbQVULJPTtGsj4gby45L15KKB/WtFg16J2Z4Tr/Z0y5R9QQnhkzbgocSU0GM8uyjeq3CwBoyvre+fw8WePqWf4/I5woh++rqETSOccoYR4UBZ73QAfdTVPtz3iceFsXQF6TTOHTGJqXV2vWRpmiF9RXjyvL2qnyZ3BQZyoagQPFBnAahsoCRpjdpcAjCv1rHPvr75tUJ66Jus62yORSRfWq7L+hXkk3ny1hXQBQzFPGMBPpPAjVX5eBgOgNrC7qUjo+4IuIbgKdUhXUjoQTTCJGYC9/pd3aX6V6Bwlp8QScRm2DMZEwU+XkEn1yT+bB147gjaymbGSR9Msia6ZWbARm/k4ZHCatQ9AkrJegaygu6j1ocsCyfLY5s+KdMFeq3nxbpYjI/5TEG5u5yG5t1OMEBT1Dgna4CIRZhHueJypCnvCUEfuBiuToq0YNBJabdrClXyPam1dClnmoyczfFaoByBkRslzeH6vs1AzjjAWiz/KHxrEnPHopSJ/qjchn9BaIGFkwQgiugHX0DoKRfoHDaAqtvnup3I+tewLwPfClSRsIJw17NhXPPaE24Dl8YjSyo5qQ75KIGVvRXGmqb4NbAOydr1+8CYTZgeYYFxe3dUCnDKvVoHSFgLHgAU+s5nmPWw3hnL1YHkGz3xws6MChgDkrBUnwwQ16N32M4klyD+o64WloveVoP8DBzD7OcpZOzGqvW6SRXSnjMZRmTpNj05kBL4f6II9yIxuMJtie8mpHPZpoxmsDHmmsv3w5vdobULpAa6RPvrGX7wcRE1L8taBJbZSxtczl2qbn/+bfvvuyd/GFJ1gzE59Lll088EwCBNkBQiWcc8x42wy505TZucrwBhSc5VeLCF+iQuciU6Qi9psTgupdVcbo0zmUVwdmfe3xtGcxkSMff28GcGZ8MvR0ej6DnDob3jtnUx+Fj4B5c5YwkOUXleTfcG19JrQWYqYV/4P8EPsLYj7H8jWW6CqHC5lBaJrwbiVWelpgSqvF++LzIVgqTj03CnPcYQTc33hlfI5eVLD4DLQVhFx4Fo6WoUqvI58gAXttVochQ6JxxroPlooFZPZYbmK959Xl7wobqneRAKx8F4TLZZCE5muuw6zaUPAJHaWRaERPMH1tpwM57N8h3pFYk5Ih15liq7LpOjHyvh4ooZfsNc68DQs/rA8R7yi4elTcum1EeLD36JLz6NflcNfZtrODhbdMhHteT2cQJCYEwL5rOYRoGkjKgQnwJrNN+g3GlVHY6Urvcs55Asakn4vhEE7bvTyHdOppWObx5ZIm0kkeYGehsypRZFZlcB1j7eIzLBa4E5VRg0+MoZdgT0gBD7kH9cWhIJK5DQXu9hhF3oIOjARe8b6zst74im5GH1R/driLYPLmelTPUD+Arwt6Dq7hBnuAfWc2glXKBfK+mogmrPQTUsRtPyQqO6ZdrhpCkeM9IDL37QbBlRxJyfjHWdsGQCBFmBC5wVfOWAmO2IoBy2mvHkn78sr4JIf29Lvo8Mm1xSiJY5F9JuqAfVO2ARZF/wy/3uCfiMFPr5ZgLzTPC7CMfh3aUD7q36nOzWG0krXT6TsfNZ+7h7wupt4Td4WHQOSVNruLAA969MA9JzCrsC4EWRF2E6dG8ZTseivX9TG+dJAK/8Y8vpFf0qVUcnwQhtvBanvyJH0w9ZHziYRytJWcDVFXVkTlL8uxMJ86rEtwQg6vuhhrUtzqORMhfBuMxaRb4GoOV74NgndmTkTdC3QsVhKe/HVWp8P0GiOFO2JlvBBg0scgENo1S8LA=' \
+  -H 'Content-Type: application/json;charset=UTF-8' \
+  -H 'Cookie: visible=false; username=zhanzhiqiang; JSESSIONID=fac6c468-cb3a-4cfb-925f-45cf7dfb63db; rememberMe=XOn3FyReQgSgtzSkD5JZFP3OlnUP3h0msm99UjsV+ipZh4rX5+t8c+ZIn01jei4cRm7StADQzJV58NL50P8dsmxuSAtuY8hUG65p0dJT6k5eIN/g9+7F5J5vu3IYKUgIf/qvSpA2nZ8iOvhb2I07i09OQZuvK1TMkOMAvZ5SmdYmrD7sGLTavBuaz//kzNBIeWfM3sPVNAOHSODBuBcXWJSSiThGZIloynKs1u6GHdStKpOMA7JIWCupzpPLgMznQ3Uz9t7swh7sG9e+Em4wpGP3R7Z4JVd3zn75E7w+xN+JeqsxvloUpxJXgIiEA6js8ZfpRdcMDIJyvCQWrLyOp04lKqHP1Uycf9OK1w7vDfl8fbf3sEjsDYJYDrv01AouydbvIRiYHeUAWjANx4wNIINwF5GYY7b2ULrC9Gu82mLZ8tXghPxTzzBhROGd0el4tGgMDtD45X6ZlzaJ20dzK/FAMBKVVCRdenAPO8netj7nhYOgHOS4J94ab3XYc4bqA3dteD2m0UVG2NmcYvqwaWflPfPSQJMsSfkgymsQs5Rqx7kWaB4UDu93Y8yWGxBYF0RDhMt80DVTzrh2juiKRxE1K6tho5xPZE2E1R9k8qZK6UC/WX3Xz5uoOFzg/gnZlT3CnVEhaIJjSRrfEPB09thiNY6ATFISnEGjqX+Fuf25dcKBnKvkvBDIruAICWnKlXjdUgXUr6kO84lzUJXY9SYEtMf+jZeISnv36ZL316tZKF3FSZ3Ya76xCaughAGnPaenqjSpBM2mnCUD/m7mzK8PcibQhF5SWJvYopwY87CYIFH7CgZJEU5c+L319hxi3DaYDSMeY3r28QdAHmhN2R5qxgT1qYxXO+fi61FUrB6mZvrnRcYr9G2Itil4FJH0kqcG+r1amBSqsZLt4E+ArvsYF8azzWC5vpyDbFfzPeqcxo27oF9Af+W5ccGjSv1Ke1yz9NI3h7uJ3fYefkikqwrWFWcK3mj6V1zEM6b0+HbDGVsGnvoQwTi2yBLvDOF3O/15ig5Pu4j8426iYDITUc76lO34bth2iaynOPlutdv0aKD6ozxNETbUHWGTGrph2/5lMxhsK4CkJ7BDMnU64p6ZNDuiHZ1kq1ynWQlmwQzgVkqvLlVWw0HuWN2lQT1pHXWs4mWk2Loqvj9+vPW7rFO5mRjMf7KE7LbEn4UVvj4Z5/wr8uOyBaLdoNt1R4ft6WKY0i4RRlF/SRtutQQLORndVbG5dY+OsJQyLVVDEWbuqEXNVXeVZcic5OZrcVPgrMDnfR62oTPKrXJlUcGrzLKkIRqKxcvaCpyR8v+cX3nw+thBt+LxFO0Fhd1VVYptKASdnd6ZhbdFlTmnh64nyuOoi2dwm2oKXItk/WVviZmzHH2/QeOc66OZipXI7H0A9pjY+llcl12Ezx7WVCtj3B9B50szyySQAz2dQ6p4IWktfNK5/DCUWhiuyewvknh7gG9miW1hFW8cqum5LE9yLHbWFfJxv7n4U9e4X2wUocKLsyBi0ol8LCuBH3CiDjSeNeZTCxz5LYEDklpWZb/BeZn5dmmCNirMiFTq8D5jE5X6k1pHCmqP28jNU2PJ0wBrLrYA5/qZkk9kCw6yjThvBzHWz2aKC6cc5dGsitSrEUPyzEDYeO3TpLM4JMqzL5TfxAV+fdMfkwUqEPKY9nw9hAnEAsUUJPMEsj93I3Kc5Dbysodx7nUSB5jlpPpagZ3t7/c9PVMhjLDBT417a/83B1ebairXqX0lkBQtR4sqkfIRPanAXxf9OTnhRSWvIfHTBBnRVJrGx7UiRUss2FHpP3qslmGuWBghKRuldq48xTkXR29Ua2PZlKXhcdZbEQFfRGLKsTkEu/RDBtXwSVQuKP3DtG8IvlGg6mM+Pu5POmF7ptrF0vRYiSUnZnkdytb0+EjNafk0kNLGWwx5Hn4xJNLNyf2mWNY3NgPRd2ED9WCQw6+skVBHgAiy44Hy2Mpm2Pm/uEszkmo6czMpTpXaRM8E9AGYMtTD6H5mQ3Km4oozfz4D+XWvdvOPImVIqfQdhw3ow8+VxwTEmTkZXqWD/tblMBPU2DCgJsLQ5dHHBJZMYXxPkOtSBc4XPSUizujm7QxnaRLrbvlLYDwGM7xxH+mFh4Nqhsxq5Grq0l2n88EXLMpqLjG0rNvnFwoTePp53CvTYmhbc+Gf9edoFr+ZtR1BnRM2ouOFUGKu/vv6v+C0gptzdmirD18Qn374uSPnDTgHFZRqzMNyiR7g/bAOEtX+NO6sMsn3mb2ax5D0ZgExmGSb06clYbmVv3S2qquEG9wiHAUdtRFCHoJZdj0NNsP8HOlb4CMIL6JRgfpEOih2y/gRcgarSjbb1JMCNDiFhAZC/hCLbn04isEMjvZe0U3WTqkkJXCwGlotwZ9WyBvPhcp7XtIytHM56W8wbLOOu4HMjapk/ALLuzSNwqNZhycI2jftaK8HyHDHT5RHHCgp6BYIH3aPdSfPU6tM5Q/RKfFqbzRh5E3W4KiBDzb+vCJqz85a++uqyDDZTBw49oTkv2USMpHxosSd6zdMXhN49ExlfZfxDXMqO8EtLBTVTw+qpKMgzCd9BCP6HFNsp16a6kkKf1aQT4Hf183EoJJiSZPnxHPCt6A6SSwLTI01/o3Ca0HfXI6ZVmk=' \
   -H 'Origin: https://aidev.haiersmarthomes.com' \
   -H 'Pragma: no-cache' \
-  -H 'Referer: https://aidev.haiersmarthomes.com/xuanwu-admin/ver/deployTask/add' \
+  -H 'Referer: https://aidev.haiersmarthomes.com/skills/upload/workbench/skillIndex.html' \
   -H 'Sec-Fetch-Dest: empty' \
   -H 'Sec-Fetch-Mode: cors' \
   -H 'Sec-Fetch-Site: same-origin' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
-  -H 'X-Requested-With: XMLHttpRequest' \
-  -H 'sec-ch-ua: "Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "macOS"' \
-  --data-raw 'version=2.62.72&remark=fb' \
-  --compressed"""
+  -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36' \
+  -H 'sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"' \
+  -H 'sec-ch-ua-mobile: ?1' \
+  -H 'sec-ch-ua-platform: "Android"' \
+  --data-raw '{"id":"","domainCode":"IntegratedStove","intentCode":"openSpinWash","intentName":"打开自旋洗","commandCheckFlag":"N","statusQueryFlag":"N","functionQueryFlag":"N","functionStatusQueryFlag":"N","extend":"{}"}'"""
 
     code = convert_curl_to_python_request(curl_code)
     print(code)
